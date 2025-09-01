e840864ca80ed591fe71172c85120c42c5163a2a
meanly <63057881+meanly@users.noreply.github.com>
2025-09-01 13:40:46 +0800

    initial

---
+++ Assets/QuestTextFlashlight.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class QuestUIManager : MonoBehaviour
+{
+    public TMP_Text questText;
+
+    // Drag the new conversation object here in the Inspector
+    public GameObject QuestDoneActivator;
+    public GameObject QuestActivateDelete;
+
+    private void Start()
+    {
+        questText.gameObject.SetActive(false);
+    }
+
+    public void ActivateQuestText()
+    {
+        questText.text = "Objective: Hanapin ang flashlight at kausapin uli si Kapitan.";
+        questText.gameObject.SetActive(true);
+    }
+
+    public void CompleteQuestText()
+    {
+        questText.text = " ";
+        questText.gameObject.SetActive(false);
+    }
+
+    public void ChangeConvo()
+
+    {
+        QuestDoneActivator.gameObject.SetActive(true);
+        QuestActivateDelete.gameObject.SetActive(false);
+    }
+}
+                                                 
